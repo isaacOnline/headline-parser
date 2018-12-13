a2e8c6d48669fa02e9961fe54c8450412f2ac224
David McClure <davidwilliammcclure@gmail.com>
2018-12-12 22:33:05 -0500

    Docs, update src.

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 
 # Headline parser
 
-This library extracts the longest coherent "sentence" (or sentences) from a news article headline, with the goal of removing paratext around the actual content. Eg, headlines will often look like:
+Standardize news article headlines. As much as possible, the goal is to remove any element of content-independent "house style." Eg, if every AP headline looks like:
 
-- Analysis | **What you should know when thinking about becoming a landlord in retirement**
-- Opinion | **The Mother’s Day Trap**
-- **Afghan envoy: We're fighting, but need help** - CNN Video
-- The Latest: **Britain says US wrong to separate migrant kids**
+> AP News: Today A and B happened...
+> AP News: Yesterday X and Y happened...
 
-Where, what we really care about is the bold stuff, and the outlet / "desk" labels are just noise that appears over and over again.
+We don't care about things like the `AP News:` prefix, just the actual headline.
+
+Steps:
+1. Standardize quotes, hyphens; drop Twitter `@` and `#`.
+1. Break on any kind of "separator" characters that don't appear in vanilla English sentences - `:`, `|`, `~`, etc. And the word `via`, which is used by some news outlets at the end of headlines to identify the source - `... via @dailycaller`.
+1. Take the longest sequence of unbroken tokens.
+1. Clean the tokens - downcase, strip punctuation. Keep `$`, but replace `[0-9]+` digits with a single `#` character, since different outlets have different conventions for how numbers are reported / formatted.
+
+Generally, if we imagine a classifier that tries to predict `(headling -> outlet)`, the idea is to get rid of anything that would give the model an "unfair advantage," and force it to just consider the raw linguistic content of the headline.
--- headline_parser.py
+++ headline_parser.py
@@ -7,18 +7,18 @@ import regex
 from spacy.tokens import Token, Doc
 from boltons.iterutils import pairwise
 from functools import reduce
-from cached_property import cached_property
 
 
-CURLY_STRAIGHT = (('“', '"'), ('”', '"'), ('‘', "'"), ('’', "'"))
+# TODO: What to do with "/"?
+
+
+QUOTES = (('“', '"'), ('”', '"'), ('‘', "'"), ('’', "'"))
 
 def standardize_quotes(text):
     """Curly -> straight.
     """
-    for c, s in CURLY_STRAIGHT:
-        text = text.replace(c, s)
-
-    text = text.replace("''", '"')
+    for special, standard in QUOTES:
+        text = text.replace(special, standard)
 
     return text
 
@@ -38,21 +38,24 @@ def standardize_hyphens(text):
     return text
 
 
-def scrub_twitter_chars(text):
+def drop_twitter_chars(text):
     return re.sub('[@#]', '', text)
 
 
 STANDARDIZERS = (
     standardize_quotes,
     standardize_hyphens,
-    scrub_twitter_chars,
+    drop_twitter_chars,
 )
 
 def standardize_text(text):
     return reduce(lambda t, func: func(t), STANDARDIZERS, text)
 
 
+# Headlines can contain letters, numbers, ".,;", "?!", $, and spaces.
 BREAK_CHAR_PATTERN = '[^a-z0-9\.,;\'"\?!\$\s]'
+
+# Regular tokens that constitute a break.
 ALPHA_BREAK_TOKENS = {'via'}
 
 def is_break_token(token):
@@ -66,26 +69,36 @@ def is_break_token(token):
     return has_break_char or is_alpha_break_token
 
 
-def clean_clf_text(token):
+# For the classifier, drop everything except letters, numbers, and $.
+CLF_REMOVED_CHAR_PATTERN = '[^a-z0-9\$]'
+
+def text_clf(token):
     """Drop everything but letters, numbers, and currency ($.,)
     """
-    text = re.sub('[^a-z0-9\$\.,]', '', token.text.lower())
+    # Drop everything but letters, numbers, $.
+    text = re.sub(CLF_REMOVED_CHAR_PATTERN, '', token.text.lower())
 
-    # Drop periods, unless in number.
-    if not re.match('[0-9\.,]', text):
-        text = re.sub('[\.,]', '', text)
+    # Digits -> '#'.
+    text = re.sub('[0-9]+', '#', text)
 
     return text
+
+
+def is_clf_token(token):
+    return len(token._.text_clf)
 
 
 def longest_unbroken_span(doc):
     """Find the longest span of tokens without a break token.
     """
+    # Left boundary, break indexes, right boundary.
     break_idxs = [-1, *[t.i for t in doc if t._.is_break_token], len(doc)]
 
+    # Get longest span.
     pairs = sorted(pairwise(break_idxs), key=lambda p: p[1]-p[0])
     i1, i2 = pairs[-1]
 
+    # Slice tokens inside of (but not including) the breaks.
     return doc[i1+1:i2]
 
 
@@ -93,37 +106,26 @@ def clf_tokens(doc):
     """Get filtered tokens for classifier.
     """
     return [
-        t._.text_clf for t in doc._.longest_unbroken_span
-        if len(t._.text_clf)
+        t for t in doc._.longest_unbroken_span
+        if t._.is_clf_token
     ]
 
 
+def clf_token_texts(doc):
+    return [t._.text_clf for t in doc._.clf_tokens]
+
+
+
 Token.set_extension('is_break_token', getter=is_break_token)
-Token.set_extension('text_clf', getter=clean_clf_text)
+Token.set_extension('text_clf', getter=text_clf)
+Token.set_extension('is_clf_token', getter=is_clf_token)
 
 Doc.set_extension('longest_unbroken_span', getter=longest_unbroken_span)
 Doc.set_extension('clf_tokens', getter=clf_tokens)
+Doc.set_extension('clf_token_texts', getter=clf_token_texts)
 
 nlp = spacy.load('en', disable=['ner', 'parser', 'tagger'])
 
 
-class Headline:
-
-    def __init__(self, text):
-        self.text = text
-
-    @cached_property
-    def standardized_text(self):
-        return standardize_text(self.text)
-
-    @cached_property
-    def doc(self):
-        return nlp(self.standardized_text)
-
-    @cached_property
-    def tokens(self):
-        return [t.text for t in self.doc]
-
-    @cached_property
-    def clf_tokens(self):
-        return self.doc._.clf_tokens
+def parse_headline(text):
+    return nlp(standardize_text(text))
